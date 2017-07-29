aa461b9413df85af0973bf65873c6916c7ae79c9
muhammed dea'a aldeen <hamdongunner@gmail.com>
2017-07-29 16:39:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ require("csv-to-array")({
             times.push(array[i].date[0] + array[i].date[1] + array[i].date[2] + array[i].date[3] + array[i].date[4] + array[i].date[5] + array[i].date[6] + array[i].date[8] + array[i].date[7] + array[i].date[8] + array[i].date[9] + array[i].date[10] + array[i].date[11] + array[i].date[12] + array[i].date[13] + array[i].date[14] + array[i].date[15]);
             callers.push(array[i].callerid);
             if(array[i].status === "ANSWERED"){
-               relashin.push(array[i].callerid + array[i].employeeid);
-                mostP.push(array[i].employeeid + array[i].status);
+               relashin.push(array[i].callerid + " "+ array[i].employeeid);
+                mostP.push(array[i].employeeid);
             }else{
                 leastP.push(array[i].employeeid + " didnt answerd");
             }
